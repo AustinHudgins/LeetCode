28affa91f2f91fd0f832569a82420be804962b3f
AustinHudgins <austin.hudgins@gmail.com>
2023-12-21 10:38:23 -0500

    removedduplicates has bug

--- LeetCode.java
+++ LeetCode.java
@@ -3,13 +3,14 @@ public class LeetCode
 {
     public static void main(String[] args)
     {
-        int[] nums1 = {0};
+        int[] nums1 = {0,0,0,0,1,1,3,4,6,6,7};
         int m = 0;
         int[] nums2 = {1};
         int n = 1;
        
         
-        Problems.MergeSortedArray.merge(nums1,m,nums2,n);
+        //Problems.MergeSortedArray.merge(nums1,m,nums2,n);
+        Problems.RemoveDuplicatesFromSortedArray.removeDuplicates(nums1);
      
     }   
 }
